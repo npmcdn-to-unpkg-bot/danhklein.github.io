b49df10d58565b2c04810fda7e19f58b60d06a2d
Daniel Klein <danhirschklein@gmail.com>
2016-04-25 11:48:43 -0600

    commented out main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,54 @@
-// add scripts
+// // add scripts
 
-console.log("sanity check!");
-function onFormSubmit() {
-  'use strict';
+// console.log("sanity check!");
+// function onFormSubmit() {
+//   'use strict';
 
-  let options = {filters: []};
+//   let options = {filters: []};
 
-  let filterService = document.getElementById('service').value;
-  if (filterService.startsWith('0x')) {
-    filterService = parseInt(filterService, 16);
-  }
-  if (filterService) {
-    options.filters.push({services: [filterService]});
-  }
+//   let filterService = document.getElementById('service').value;
+//   if (filterService.startsWith('0x')) {
+//     filterService = parseInt(filterService, 16);
+//   }
+//   if (filterService) {
+//     options.filters.push({services: [filterService]});
+//   }
 
-  let filterName = document.getElementById('name').value;
-  if (filterName) {
-    options.filters.push({name: filterName});
-  }
+//   let filterName = document.getElementById('name').value;
+//   if (filterName) {
+//     options.filters.push({name: filterName});
+//   }
 
-  let filterNamePrefix = document.getElementById('namePrefix').value;
-  if (filterNamePrefix) {
-    options.filters.push({namePrefix: filterNamePrefix});
-  }
+//   let filterNamePrefix = document.getElementById('namePrefix').value;
+//   if (filterNamePrefix) {
+//     options.filters.push({namePrefix: filterNamePrefix});
+//   }
 
-  log('Requesting Bluetooth Device...');
-  navigator.bluetooth.requestDevice(options)
-  .then(device => {
-    log('> Name:             ' + device.name);
-    log('> Id:               ' + device.id);
-    log('> UUIDs:            ' + device.uuids.join('\n' + ' '.repeat(20)));
-    log('> Connected:        ' + device.gatt.connected);
-    if (device.adData) {
-      log('> Tx Power:         ' + device.adData.txPower + ' dBm');
-      log('> RSSI:             ' + device.adData.rssi + ' dBm');
-    }
-  })
-  .catch(error => {
-    log('Argh! ' + error);
-  });
-}
+//   log('Requesting Bluetooth Device...');
+//   navigator.bluetooth.requestDevice(options)
+//   .then(device => {
+//     log('> Name:             ' + device.name);
+//     log('> Id:               ' + device.id);
+//     log('> UUIDs:            ' + device.uuids.join('\n' + ' '.repeat(20)));
+//     log('> Connected:        ' + device.gatt.connected);
+//     if (device.adData) {
+//       log('> Tx Power:         ' + device.adData.txPower + ' dBm');
+//       log('> RSSI:             ' + device.adData.rssi + ' dBm');
+//     }
+//   })
+//   .catch(error => {
+//     log('Argh! ' + error);
+//   });
+// }
 
-  log = ChromeSamples.log;
+//   log = ChromeSamples.log;
 
-  function isWebBluetoothEnabled() {
-    if (navigator.bluetooth) {
-      return true;
-    } else {
-      ChromeSamples.setStatus('Web Bluetooth API is not available.\n' +
-                              'Please make sure the Web Bluetooth flag is enabled.');
-      return false;
-    }
+//   function isWebBluetoothEnabled() {
+//     if (navigator.bluetooth) {
+//       return true;
+//     } else {
+//       ChromeSamples.setStatus('Web Bluetooth API is not available.\n' +
+//                               'Please make sure the Web Bluetooth flag is enabled.');
+//       return false;
+//     }
 
